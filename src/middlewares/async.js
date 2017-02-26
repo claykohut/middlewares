11080a5febfaec726a008882e628ddba0389c172
Clay Kohut <claykohut@MacBook-Pro-3.local>
2017-02-26 01:14:44 -0500

    got basic middleware to resolve promises

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,23 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log('action: ', action);
+    // if action does not have a payload
+    // or, if the payload does not have a .then property
+    // we dont care about it, send it on
+    if(!action.payload || !action.payload.then) {
+      console.log('dont care about this action! ', action)
+      return next(action)
+    }
 
-    next(action)
+    console.log('waiting for promise')
+    // Make sure the action's promise resolves
+    action.payload
+      .then((response) => {
+        const newAction = {
+          ...action,
+          payload: response.data
+        }
+        // dispatch new (modified) action through all middleware
+        dispatch(newAction)
+      })
   }
 }
